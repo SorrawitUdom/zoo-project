02cc5955512421e92ce9b363ce925258efb3bd55
Sorrawit Udomvittayakrai <119516986+SorrawitUdom@users.noreply.github.com>
2024-11-18 07:56:52 +0000

    Fix wrong price assignment

--- test_zoo.py
+++ test_zoo.py
@@ -5,10 +5,21 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_notvalid(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), None)
+
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
        
-    # Add your additional test cases here.
+    # Add your additional test cases here
+    def test_teenager_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+    
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_elder_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
 
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if 0 <= age <= 12:
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20:
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60:
             return 150
-        elif age >= 60:
+        elif age > 60: #chang from age >= 60 to age > 60 [This line does not trigger any abnormally]
             return 100
+        else :
+            return None
